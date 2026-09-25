38869257baf47bb3a8825b2e4c718540bdd14149
Gutha Yaswanth <guthayaswanth@gmail.com>
2026-09-25 17:03:19 +0530

    DOM Manipulation

--- FrontendLearning/HTML/F1.js
+++ FrontendLearning/HTML/F1.js
@@ -1,9 +1,19 @@
-function login(){
-    let mail=document.getElementById("email").value;
-    let password=document.getElementById("password").value;
-    if(mail=== "" || password===""){
-        document.getElementById("msg").innerText="All feilds are required";
-    }else{
-        document.getElementById("msg").innerText="Login Sucessfull";
-    }
-}
+// function login(){
+//     let mail=document.getElementById("email").value;
+//     let password=document.getElementById("password").value;
+//     if(mail=== "" || password===""){
+//         document.getElementById("msg").innerText="All feilds are required";
+//     }else{
+//         document.getElementById("msg").innerText="Login Sucessfull";
+//     }
+// }
+
+
+let buttons = document.querySelectorAll("button");
+let message = document.getElementById("message");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        message.textContent = this.textContent;
+    });
+});
